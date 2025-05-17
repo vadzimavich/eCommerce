dcceeef1ff805a11f82bc7d937f85eddc325efaf
DenisNedelko <dnsndlk@gmail.com>
2025-05-18 02:02:58 +0300

    refactor: change icons logo and styles 2025-05-18 02:02:58

--- src/views/components/Header/headerView.ts
+++ src/views/components/Header/headerView.ts
@@ -3,7 +3,7 @@ import { AppModel } from '../../../models/state/AppState';
 import { elementCreator } from '../../../utils/dom-helpers';
 
 export class HeaderView {
-  private headerContainer: HTMLElement;
+  private header: HTMLElement;
   private navContainer: HTMLElement;
   private logoContainer: HTMLElement;
   private cartIconContainer: HTMLElement;
@@ -11,20 +11,24 @@ export class HeaderView {
   private logoutContainer: HTMLElement;
 
   constructor(private readonly appModel: AppModel) {
-    this.headerContainer = elementCreator(document.createElement('header'), { classNames: ['header'] });
+    this.header = elementCreator(document.createElement('header'), { classNames: ['header'] });
     this.navContainer = elementCreator(document.createElement('nav'), { classNames: ['header__nav', 'nav'] });
-    this.logoContainer = elementCreator(document.createElement('div'), { classNames: ['header__logo', 'logo'] });
+    this.logoContainer = elementCreator(document.createElement('h1'), { classNames: ['header__logo', 'logo'] });
     this.cartIconContainer = elementCreator(document.createElement('div'), {
-      classNames: ['header__cart', 'header__cart-wrapper'],
+      classNames: ['header__cart', 'nav-user__cart-wrapper'],
     });
-    this.currentUserHead = elementCreator(document.createElement('span'), { classNames: ['header__user'] });
-    this.logoutContainer = elementCreator(document.createElement('div'), { classNames: ['header__logout'] });
+    this.currentUserHead = elementCreator(document.createElement('span'), {
+      classNames: ['header__user', 'nav-user__user'],
+    });
+    this.logoutContainer = elementCreator(document.createElement('div'), {
+      classNames: ['header__logout', 'nav-user__logout-wrapper'],
+    });
   }
 
   public render(): HTMLElement {
     const header = this.buildHeader();
-    this.headerContainer.append(header);
-    return this.headerContainer;
+    this.header.append(header);
+    return this.header;
   }
 
   public getNavContainer(): HTMLElement {
@@ -64,7 +68,7 @@ export class HeaderView {
       attributes: { 'data-route': '/home', href: '#/home' },
     });
     const logoImg = elementCreator(document.createElement('img'), {
-      classNames: ['logo', 'header__logo', 'header__logo-img'],
+      classNames: ['logo', 'header__logo-img'],
       attributes: { src: '../assets/logo/logo.png', alt: 'app-logo' },
     });
     anchorItem.append(logoImg);
@@ -82,11 +86,13 @@ export class HeaderView {
       { label: 'Sign Up', route: '/sign-up' },
     ];
 
-    const navList = elementCreator(document.createElement('ul'), { classNames: ['nav-list'] });
-    const userItem = elementCreator(document.createElement('ul'), { classNames: ['nav-list'] });
+    const navList = elementCreator(document.createElement('ul'), { classNames: ['header__nav-list'] });
+    const userItem = elementCreator(document.createElement('ul'), { classNames: ['header__nav-user'] });
 
     items.forEach((item) => {
-      const navListItem = elementCreator(document.createElement('li'), { classNames: ['nav-item', 'nav-list__item'] });
+      const navListItem = elementCreator(document.createElement('li'), {
+        classNames: ['nav-list__item'],
+      });
       const anchorItem = elementCreator(document.createElement('a'), {
         content: item.label,
         attributes: { 'data-route': item.route, href: `#${item.route}` },
@@ -94,7 +100,9 @@ export class HeaderView {
       navListItem.append(anchorItem);
       navList.append(navListItem);
     });
-    userItem.append(this.buildUserContainer());
+    const cart = this.buildCartElement();
+    const logout = this.buildLogoutElement();
+    userItem.append(cart, this.currentUserHead, logout);
     this.navContainer.append(navList, userItem);
   }
 
@@ -102,9 +110,9 @@ export class HeaderView {
     const anchorItem = elementCreator(document.createElement('a'), {
       attributes: { 'data-route': '/cart', href: '#/cart', title: 'cart' },
     });
-    const iconCart = elementCreator(document.createElement('img'), {
-      classNames: ['header__cart-icon'],
-      attributes: { src: '../assets/icons/header-icons/header-cart.svg', alt: 'cart-icon' },
+    const iconCart = elementCreator(document.createElement('svg'), {
+      classNames: ['nav-user__cart-icon', 'header__icon', 'header__icon-cart'],
+      attributes: { alt: 'cart-icon' },
     });
     anchorItem.append(iconCart);
     this.cartIconContainer.append(anchorItem);
@@ -115,9 +123,9 @@ export class HeaderView {
     const anchorItem = elementCreator(document.createElement('a'), {
       attributes: { 'data-route': '/home', href: '#/home', title: 'logout' },
     });
-    const iconLogout = elementCreator(document.createElement('img'), {
-      classNames: ['header__logout-icon'],
-      attributes: { src: '../assets/icons/header-icons/header-logout.svg', alt: 'logout-icon' },
+    const iconLogout = elementCreator(document.createElement('svg'), {
+      classNames: ['header__logout-icon', 'header__icon', 'header__icon-logout'],
+      attributes: {},
     });
     anchorItem.append(iconLogout);
     this.logoutContainer.append(anchorItem);
@@ -130,13 +138,5 @@ export class HeaderView {
     const logo = this.buildLogoElement();
     headerWrapper.append(logo, this.navContainer);
     return headerWrapper;
-  }
-
-  private buildUserContainer(): HTMLElement {
-    const userContainer = elementCreator(document.createElement('div'), { classNames: ['header__user-wrapper'] });
-    const cart = this.buildCartElement();
-    const logout = this.buildLogoutElement();
-    userContainer.append(cart, this.currentUserHead, logout);
-    return userContainer;
   }
 }
