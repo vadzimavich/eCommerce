3bbf4ab09624bcf92d74203f002e939a80befbc2
DenisNedelko <dnsndlk@gmail.com>
2025-05-18 19:00:03 +0300

    refactor: changed style 2025-05-18 19:00:03

--- src/views/pages/AboutUsPage/index.ts
+++ src/views/pages/AboutUsPage/index.ts
@@ -1,6 +1,7 @@
 export class AboutPage {
   public render(): HTMLElement {
     const container = document.createElement('div');
+    // Заглушка класс для страниц потом удалим
     container.classList.add('plug');
     container.innerHTML = 'aboutPage';
 
--- src/views/pages/BasketPage/index.ts
+++ src/views/pages/BasketPage/index.ts
@@ -1,6 +1,7 @@
 export class CartPage {
   public render(): HTMLElement {
     const container = document.createElement('div');
+    // Заглушка класс для страниц потом удалим
     container.classList.add('plug');
     container.innerHTML = 'cartPage';
 
