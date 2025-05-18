f0ccad23f8093b75cf63a23389c855d06a4cf92e
DenisNedelko <dnsndlk@gmail.com>
2025-05-19 00:55:13 +0300

    fix: fixed hash and view titles in header 2025-05-19 00:55:13

--- src/models/state/AppState.ts
+++ src/models/state/AppState.ts
@@ -2,7 +2,9 @@ import { Subscriber } from '../types';
 
 export class AppModel {
   private currentUser: string = '';
+  private currentHash: string = '/';
   private currentUserListener: Subscriber[] = [];
+  private currentPageListener: Subscriber[] = [];
 
   public setCurrentUser(user: string): void {
     this.currentUser = user;
@@ -17,15 +19,33 @@ export class AppModel {
       this.notifyUserListener();
     }
   }
+
   public getCurrentUser(): string {
     return this.currentUser;
+  }
+
+  public getCurrentRoute(): string {
+    return this.currentHash;
+  }
+
+  public setCurrentHash(hash: string): void {
+    this.currentHash = hash;
+    this.notifyCurrentPageListener();
   }
 
   public subscribeUsersListener(callback: () => void): void {
     this.currentUserListener.push(callback);
   }
 
+  public subscribeCurrentPageListener(callback: () => void): void {
+    this.currentPageListener.push(callback);
+  }
+
   private notifyUserListener(): void {
     this.currentUserListener.forEach((callback) => callback());
+  }
+
+  private notifyCurrentPageListener(): void {
+    this.currentPageListener.forEach((callback) => callback());
   }
 }
--- src/views/components/Header/headerModel.ts
+++ src/views/components/Header/headerModel.ts
@@ -1,18 +1,11 @@
 import { Subscriber } from '../../../models/types';
 
 export class HeaderModel {
-  private currentRoute: string;
   private isOpenBM: boolean;
-  private currentPageListener: Subscriber[] = [];
   private burgerMenuListeners: Subscriber[] = [];
 
   constructor() {
-    this.currentRoute = '/home';
     this.isOpenBM = false;
-  }
-
-  public getCurrentRoute(): string {
-    return this.currentRoute;
   }
 
   public getBurgerMenuState(): boolean {
@@ -24,21 +17,8 @@ export class HeaderModel {
     this.notifyBurgerMenuListeners();
   }
 
-  public setCurrentRoute(route: string): void {
-    this.currentRoute = route;
-    this.notifyCurrentPageListener();
-  }
-
-  public subscribeCurrentPageListener(callback: () => void): void {
-    this.currentPageListener.push(callback);
-  }
-
   public subscribeBurgerMenuListener(callback: () => void): void {
     this.burgerMenuListeners.push(callback);
-  }
-
-  private notifyCurrentPageListener(): void {
-    this.currentPageListener.forEach((callback) => callback());
   }
 
   private notifyBurgerMenuListeners(): void {
