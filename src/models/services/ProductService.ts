7ab92f0987b42daa9775675198f935fcda6b3b0d
DenisNedelko <dnsndlk@gmail.com>
2025-05-25 20:42:51 +0300

    feat: add product Service getAllProducts 2025-05-25 20:42:51

---
+++ src/models/services/ProductService.ts
@@ -0,0 +1,24 @@
+import type { ProductProjection } from '@commercetools/platform-sdk';
+import { CustomerService } from './AuthService';
+
+export class ProductsService {
+  private static instance: ProductsService;
+  private readonly service = CustomerService.getInstance();
+
+  public static getInstance(): ProductsService {
+    if (!ProductsService.instance) {
+      ProductsService.instance = new ProductsService();
+    }
+    return ProductsService.instance;
+  }
+
+  public async getAllProducts(): Promise<ProductProjection[] | Error> {
+    try {
+      const response = await this.service.getCurrentClient().productProjections().get().execute();
+      return response.body.results;
+    } catch (error) {
+      if (error instanceof Error) return error;
+      return new Error('Unknown registration error');
+    }
+  }
+}
--- src/views/pages/CatalogPage/catalogController.ts
+++ src/views/pages/CatalogPage/catalogController.ts
@@ -1,9 +1,14 @@
+import { ProductsService } from '../../../models/services/ProductService';
 import { CatalogModel } from './catalogModel';
 import { CatalogView } from './view/catalodView';
 
 export class CatalogController {
+  private readonly service: ProductsService;
   constructor(
     private readonly model: CatalogModel,
     private readonly view: CatalogView
-  ) {}
+  ) {
+    this.service = ProductsService.getInstance();
+    console.log('Products', this.service.getAllProducts());
+  }
 }
