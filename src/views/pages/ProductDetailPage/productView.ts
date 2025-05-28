7ae27e962ba60965b088cd72f52101944637672a
Tatsiana Haranina <tanyagoryanina.dev@gmail.com>
2025-05-28 20:22:37 +0300

    feat: implement swiper for images

--- src/views/pages/ProductDetailPage/productView.ts
+++ src/views/pages/ProductDetailPage/productView.ts
@@ -1,15 +1,23 @@
 import { elementCreator } from '../../../utils/dom-helpers';
 import { ProductModel } from './productModel';
+import Swiper from 'swiper';
+import { Navigation, Pagination, Thumbs } from 'swiper/modules';
 
 export class ProductView {
+  private swiper: HTMLDivElement;
+  private thumbsSwiper: HTMLDivElement;
   private container: HTMLElement;
-  private wrapperImages: HTMLDivElement;
 
   constructor(private readonly model: ProductModel) {
     this.container = elementCreator(document.createElement('section'), {
       classNames: ['page-wrapper', 'product'],
     });
-    this.wrapperImages = elementCreator(document.createElement('div'), { classNames: ['product__wrapper__images'] });
+    this.swiper = elementCreator(document.createElement('div'), {
+      classNames: ['product__wrapper__main-slider', 'swiper'],
+    });
+    this.thumbsSwiper = elementCreator(document.createElement('div'), {
+      classNames: ['product__wrapper__thumbs-swiper', 'swiper'],
+    });
   }
 
   public render(): HTMLElement {
@@ -35,8 +43,9 @@ export class ProductView {
   private createWrapper(): void {
     const wrapper = elementCreator(document.createElement('div'), { classNames: ['product__wrapper'] });
 
-    wrapper.append(this.createImagesWrapper(), this.createDescription());
+    wrapper.append(this.createSwiperElements(), this.createDescription());
     this.container.append(wrapper);
+    this.initializeSwiper();
   }
 
   private createDescription(): HTMLDivElement {
@@ -83,18 +92,67 @@ export class ProductView {
     return wrapper;
   }
 
-  private createImagesWrapper(): HTMLDivElement {
-    this.model.dataProduct?.images?.forEach((item) => {
-      const img = elementCreator(document.createElement('img'), {
-        classNames: ['product__img'],
-        attributes: {
-          src: item,
-          alt: this.model.dataProduct?.title || '',
-        },
-      });
-      this.wrapperImages.append(img);
+  private createSwiperElements(): HTMLDivElement {
+    const sliders = elementCreator(document.createElement('div'), {
+      classNames: ['product__wrapper__sliders'],
     });
 
-    return this.wrapperImages;
+    const wrapperSlider = elementCreator(document.createElement('div'), {
+      classNames: ['main-swiper', 'swiper-wrapper'],
+    });
+
+    const wrapperThumbsSlider = elementCreator(document.createElement('div'), {
+      classNames: ['thumbs-swiper', 'swiper-wrapper'],
+    });
+
+    this.model.dataProduct?.images?.forEach((item) => {
+      for (let i = 0; i < 2; i++) {
+        const wrapperImg = elementCreator(document.createElement('div'), {
+          classNames: ['product__wrapper__img', 'swiper-slide'],
+        });
+
+        const img = elementCreator(document.createElement('img'), {
+          classNames: ['product__img'],
+          attributes: {
+            src: item,
+            alt: this.model.dataProduct?.title || '',
+          },
+        });
+
+        wrapperImg.append(img);
+
+        if (i === 0) {
+          wrapperSlider.append(wrapperImg);
+        } else {
+          wrapperThumbsSlider.append(wrapperImg);
+        }
+      }
+    });
+
+    this.swiper.append(wrapperSlider);
+    this.thumbsSwiper.append(wrapperThumbsSlider);
+
+    sliders.append(this.swiper, this.thumbsSwiper);
+    return sliders;
+  }
+
+  private initializeSwiper(): void {
+    const thumbsSwiperInstance = new Swiper(this.thumbsSwiper, {
+      modules: [Navigation, Pagination, Thumbs],
+      spaceBetween: 10,
+      slidesPerView: 3,
+      freeMode: true,
+      watchSlidesProgress: true,
+      navigation: true,
+    });
+
+    new Swiper(this.swiper, {
+      modules: [Navigation, Pagination, Thumbs],
+      navigation: true,
+      pagination: { clickable: true },
+      thumbs: {
+        swiper: thumbsSwiperInstance,
+      },
+    });
   }
 }
