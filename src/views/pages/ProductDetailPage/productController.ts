6dc2800755921de9316724d7490d79f6ec28ea14
Tatsiana Haranina <tanyagoryanina.dev@gmail.com>
2025-05-29 01:10:51 +0300

    feat: add routing to not-found

--- src/views/pages/ProductDetailPage/productController.ts
+++ src/views/pages/ProductDetailPage/productController.ts
@@ -1,4 +1,5 @@
 //import { route } from '../../../app';
+import { route } from '../../../app';
 import { ProductsService } from '../../../models/services/ProductService';
 import { AppModel } from '../../../models/state/AppState';
 import { parseProduct } from '../../../utils/parsers';
@@ -19,14 +20,15 @@ export class ProductController {
 
   private async initProduct(): Promise<void> {
     try {
-      const data = await this.service.getProductById('364444e3-4e66-4897-aeaf-146e46edb5c1');
+      //const idProduct = window.location.hash.split('/').at(-1) || '';
+      const data = await this.service.getProductById('3a06a607-0674-44c3-8a90-a14b8e373c11');
 
       if (data && !(data instanceof Error)) {
         const parsedProducts = parseProduct(data);
         this.model.setProducts(parsedProducts);
       }
-    } catch (error) {
-      console.error('Error loading product', error);
+    } catch {
+      route.navigate('/not-found');
     }
   }
 
