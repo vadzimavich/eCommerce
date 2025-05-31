ffd6eb09e6c0507ed3029829b4dbf00c263f3113
Tatsiana Haranina <tanyagoryanina.dev@gmail.com>
2025-05-31 17:53:45 +0300

    fix: test: calculate formateTomorrow

--- src/__tests__/utils/handlers/handlerDateField.ts
+++ src/__tests__/utils/handlers/handlerDateField.ts
@@ -9,8 +9,11 @@ const getTwoDigit = (value: number): string => {
   return value > 9 ? value.toString() : '0' + value;
 };
 
-const formateToday = `${today.getFullYear()}-${getTwoDigit(month + 1)}-${day}`;
-const formateTomorrow = `${today.getFullYear()}-${getTwoDigit(month + 1)}-${getTwoDigit(day + 1)}`;
+const formateToday = `${today.getFullYear()}-${getTwoDigit(month + 1)}-${getTwoDigit(day)}`;
+
+const tomorrow = new Date(today);
+tomorrow.setDate(today.getDate() + 1);
+const formateTomorrow = `${tomorrow.getFullYear()}-${getTwoDigit(tomorrow.getMonth() + 1)}-${getTwoDigit(tomorrow.getDate())}`;
 
 const testCases = [
   {
