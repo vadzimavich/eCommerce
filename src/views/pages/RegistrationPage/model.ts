29ab2789323af2b92413a6220d960b72e9ef1533
DenisNedelko <dnsndlk@gmail.com>
2025-06-05 13:01:53 +0300

    fix: createDataRequest 2025-06-05 13:01:53

--- src/views/pages/RegistrationPage/model.ts
+++ src/views/pages/RegistrationPage/model.ts
@@ -139,13 +139,13 @@ export class RegistrationModel {
       lastName: this.dataForm['last-name'],
       dateOfBirth: this.dataForm.bday,
       shippingAddress: {
-        country: this.getCodeCountry(this.dataForm['shipping-country']),
+        country: this.dataForm['shipping-country'],
         city: this.dataForm['shipping-city'],
         streetName: this.dataForm['shipping-street'],
         postalCode: this.dataForm['shipping-postal-code'],
       },
       billingAddress: {
-        country: this.getCodeCountry(this.dataForm['billing-country']),
+        country: this.dataForm['billing-country'],
         city: this.dataForm['billing-city'],
         streetName: this.dataForm['billing-street'],
         postalCode: this.dataForm['billing-postal-code'],
@@ -156,15 +156,5 @@ export class RegistrationModel {
     };
 
     return data;
-  }
-
-  private getCodeCountry(country: string): string {
-    if (country === 'USA') {
-      return 'US';
-    } else if (country === 'Canada') {
-      return 'CA';
-    } else {
-      return '';
-    }
   }
 }
