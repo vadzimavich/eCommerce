ef721efdba69365ab48d6e023b0225e386fee399
vadzimavich <vadzimavich@gmail.com>
2025-06-16 17:51:49 +0300

    feat: implement basic cart logic (fetch & display items, display 'empty' message)

---
+++ src/views/pages/BasketPage/cartController.ts
@@ -0,0 +1,22 @@
+import { CartService } from '../../../models/services/CartService';
+import { AppModel } from '../../../models/state/AppState';
+import { CartModel } from './cartModel';
+import { CartView } from './cartView';
+
+export class CartController {
+  private cartService: CartService;
+
+  constructor(
+    private readonly appModel: AppModel,
+    private readonly cartModel: CartModel,
+    private readonly cartView: CartView
+  ) {
+    this.cartService = CartService.getInstance(this.appModel);
+    this.init();
+  }
+
+  private async init(): Promise<void> {
+    const cart = await this.cartService.getOrCreateCart();
+    this.cartModel.setCart(cart);
+  }
+}
